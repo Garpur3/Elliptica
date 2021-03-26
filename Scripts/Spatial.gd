f74168b9d91e8557dd541724a3f38b5d3e8ea67f
Garpur3 <g@rpur.net>
2021-03-26 12:34:16 +0000

    Small edits

--- Scripts/Spatial.gd
+++ Scripts/Spatial.gd
@@ -20,8 +20,8 @@ func _ready():
 			planets.append(node)
 			p_id += 1
 	
-	gman.set_planet_velocity(1, Vector3(0, 0, 1.2))
-	gman.set_planet_velocity(2, Vector3(0.8, 0, 0))
+	gman.set_planet_velocity(1, Vector3(0, 0, 100))
+	gman.set_planet_velocity(2, Vector3(50, 0, 0))
 	
 	# g = 2   ;   1 = 0,0,12   ;   2 = 8,0,0     ;    range = 12-16
 	
@@ -34,13 +34,14 @@ func _ready():
 		var theta = rng.randf_range(0, 2*PI)
 		var asteroid = steroid_template.instance()
 		asteroid.translation = Vector3(cos(theta) * R, rng.randf_range(-400, 400), sin(theta) * R)
-		var speed = asteroid.translation.normalized().cross(Vector3(0, 1, 0)) * rng.randf_range(1.2, 1.6) + Vector3(0, rng.randf_range(-1, 1), 0)
+		asteroid.scale *= rng.randf_range(0.5, 2)
+		var speed = asteroid.translation.normalized().cross(Vector3(0, 1, 0)) * rng.randf_range(12, 16) + Vector3(0, rng.randf_range(-1, 1), 0)
 		gman.add_object(i, 0, asteroid.translation, speed)
 		add_child(asteroid)
 		objects.append(asteroid)
 
 func _process(delta):
-	gman.update(delta)
+	gman.update(delta*1)
 	for i in range(planets.size()):
 		planets[i].translation = gman.get_planet_location(i)
 	for i in range(objects.size()):
